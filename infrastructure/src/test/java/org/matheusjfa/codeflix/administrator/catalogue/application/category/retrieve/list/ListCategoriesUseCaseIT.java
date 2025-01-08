5efab5fb7f5c137ee0c8ae08b717f8b763bbc8a5
Matheus José Ferreira de Aguiar <matheusjfa@outlook.com>
2025-01-08 07:55:38 -0300

    test: Improve test cases and use Parameterized tests to cover more ground with less code

--- infrastructure/src/test/java/org/matheusjfa/codeflix/administrator/catalogue/application/category/retrieve/list/ListCategoriesUseCaseIT.java
+++ infrastructure/src/test/java/org/matheusjfa/codeflix/administrator/catalogue/application/category/retrieve/list/ListCategoriesUseCaseIT.java
@@ -1,7 +1,10 @@
 package org.matheusjfa.codeflix.administrator.catalogue.application.category.retrieve.list;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.matheusjfa.codeflix.administrator.catalogue.IntegrationTest;
 import org.matheusjfa.codeflix.administrator.catalogue.domain.category.Category;
 import org.matheusjfa.codeflix.administrator.catalogue.domain.category.CategoryGateway;
@@ -25,6 +28,11 @@ public class ListCategoriesUseCaseIT {
     @SpyBean
     private CategoryGateway gateway;
 
+    @BeforeEach
+    void PopulateCategories() {
+        this.createCategories();
+    }
+
     @Test
     public void givenAValidCommand_whenCallsListCategories_thenShouldReturnCategories() {
         // Arrange
@@ -32,12 +40,6 @@ public class ListCategoriesUseCaseIT {
         final var expectedSize = 3;
         final var expectedTotalElements = 8;
         final var expectedTotalPages = 3;
-
-        Assertions.assertEquals(0, this.repository.count());
-
-        this.createCategories();
-
-        Assertions.assertEquals(8, this.repository.count());
 
         final var query = new CategorySearchQuery(0, 3, "", "name", "asc");
         final var command = ListCategoriesCommand.with(query);
@@ -59,21 +61,28 @@ public class ListCategoriesUseCaseIT {
         Assertions.assertTrue(firstElement.isActive());
     }
 
-    @Test
-    public void givenAValidCommandSortingByDescription_whenCallsListCategories_thenShouldReturnCategories() {
+    @ParameterizedTest
+    @CsvSource({
+        "asc, 0, 3, 1, 1, Ani",
+        "desc, 0, 3, 1, 1, Top",
+        "asc, 0, 3, 1, 1, Doc",
+        "desc, 0, 3, 1, 1, Ah",
+        "asc, 0, 3, 1, 1, Car",
+        "asc, 0, 3, 3, 7, Cat",
+        "asc, 2, 3, 0, 0, Invalid",
+    })
+    public void givenAValidCommandSortingByDescription_whenCallsListCategories_thenShouldReturnCategories(
+            final String expectedSortDirection,
+            final int expectedPage,
+            final int expectedPerPage,
+            final int expectedTotalElements,
+            final int expectedTotal,
+            final String expectedTerms
+    ) {
         // Arrange
-        final var expectedPage = 0;
-        final var expectedSize = 3;
-        final var expectedTotalElements = 8;
-        final var expectedTotalPages = 3;
+        final String expectedSortBy = "description";
 
-        Assertions.assertEquals(0, this.repository.count());
-
-        this.createCategories();
-
-        Assertions.assertEquals(8, this.repository.count());
-
-        final var query = new CategorySearchQuery(0, 3, "", "description", "asc");
+        final var query = new CategorySearchQuery(expectedPage, expectedPerPage, expectedTerms, expectedSortBy, expectedSortDirection);
         final var command = ListCategoriesCommand.with(query);
 
         // Act
@@ -82,48 +91,43 @@ public class ListCategoriesUseCaseIT {
         // Assert
         Assertions.assertNotNull(result);
         Assertions.assertEquals(expectedPage, result.currentPage());
-        Assertions.assertEquals(expectedSize, result.perPage());
-        Assertions.assertEquals(expectedTotalElements, result.total());
-        Assertions.assertEquals(expectedTotalPages, result.items().size());
-
-        final var firstElement = result.items().get(0);
-
-        Assertions.assertEquals("Horror Movies!", firstElement.name());
-        Assertions.assertEquals("Ah, Horror Movies!", firstElement.description());
-        Assertions.assertTrue(firstElement.isActive());
+        Assertions.assertEquals(expectedPerPage, result.perPage());
+        Assertions.assertEquals(expectedTotalElements, result.items().size());
+        Assertions.assertEquals(expectedTotal, result.total());
     }
 
-    @Test
-    public void givenAValidCommandFilteringByName_whenCallsListCategories_thenShouldReturnCategories() {
+    @ParameterizedTest
+    @CsvSource({
+            "0, 3, 3, 8, ''",
+            "0, 3, 1, 1, 'Horror Movies!'",
+            "0, 3, 0, 0, 'Invalid'",
+            "0, 3, 2, 2, 'Movies'",
+            "0, 3, 1, 1, 'Series'",
+            "1, 3, 0, 1, 'Documentary'",
+    })
+    public void givenAValidCommandFilteringByName_whenCallsListCategories_thenShouldReturnCategories(
+            final int expectedPage,
+            final int expectedPerPage,
+            final int expectedTotalElements,
+            final int expectedTotal,
+            final String expectedTerms
+    ) {
         // Arrange
-        final var expectedPage = 0;
-        final var expectedSize = 3;
-        final var expectedTotalElements = 1;
-        final var expectedTotalPages = 1;
+        final var expectedSortBy = "name";
+        final var expectedSortDirection = "asc";
 
-        Assertions.assertEquals(0, this.repository.count());
-
-        this.createCategories();
-
-        Assertions.assertEquals(8, this.repository.count());
-
-        final var query = new CategorySearchQuery(0, 3, "Animes", "name", "asc");
+        final var query = new CategorySearchQuery(expectedPage, expectedPerPage, expectedTerms, expectedSortBy, expectedSortDirection);
         final var command = ListCategoriesCommand.with(query);
         // Act
         final var result = useCase.execute(command);
+        System.out.println(result);
 
         // Assert
         Assertions.assertNotNull(result);
         Assertions.assertEquals(expectedPage, result.currentPage());
-        Assertions.assertEquals(expectedSize, result.perPage());
-        Assertions.assertEquals(expectedTotalElements, result.total());
-        Assertions.assertEquals(expectedTotalPages, result.items().size());
-
-        final var firstElement = result.items().get(0);
-
-        Assertions.assertEquals("Animes", firstElement.name());
-        Assertions.assertEquals("Anime Category", firstElement.description());
-        Assertions.assertTrue(firstElement.isActive());
+        Assertions.assertEquals(expectedPerPage, result.perPage());
+        Assertions.assertEquals(expectedTotalElements, result.items().size());
+        Assertions.assertEquals(expectedTotal, result.total());
     }
 
     @Test
@@ -160,7 +164,7 @@ public class ListCategoriesUseCaseIT {
     }
 
     @Test
-    public void givenAInvalidQueryTermToSearch_whenCallsListCategories_thenShouldReturnCategories() {
+    public void givenAInvalidQueryTermToSearch_whenCallsListCategories_thenShouldReturnEmptyListOfCategories() {
         // Arrange
         final var expectedPage = 0;
         final var expectedSize = 3;
